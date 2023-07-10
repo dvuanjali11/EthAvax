452de45b1980afdc3a0ba0c9c7887b1e40a3d1e1
dvuanjali11 <114914377+dvuanjali11@users.noreply.github.com>
2023-07-10 11:37:31 +0530

    Errorhandling.sol

--- Errorhandling.sol
+++ Errorhandling.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 contract ErrorHandling {
    
     // uint public num = 0;
-    uint b=5;
+    uint b=10;
 
     function testAssert(uint num) public pure{
         assert(num!=0);
@@ -13,17 +13,13 @@ contract ErrorHandling {
     function divide(uint _numerator, uint _denomenator) public pure returns (uint){
         if(_numerator<_denomenator){
            
-            revert("please provide numerator greater than denomenator");
+            revert("numerator shoul be greater than denomenator");
             
         }
         return _numerator/_denomenator;
-       
-
     }
     function mult(uint a) public view returns (uint){
-        require(a>0,"Value of a is zero , we don't want the result to be zero");
+        require(a>0,"Value of a must not be zero.");
         return a*b;
-
     }
-
 }
